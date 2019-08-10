09d692ea2ceff44b28af1735924317e16f48c286
Paulo Schneider <paulo.schneider@gmail.com>
2019-08-10 10:55:49 +0100

    Fake OGS serves first page of user games

--- ogsclient/fake_ogs_server_test.go
+++ ogsclient/fake_ogs_server_test.go
@@ -14,13 +14,21 @@ func fakeOgsServer(t *testing.T) *httptest.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/me", func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "GET", r.Method)
-		response, err := ioutil.ReadFile(filepath.Join("testdata", "user_resource.json"))
-		if err != nil {
-			t.Fatal(err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(response)
+		serveJson(w, filepath.Join("testdata", "user_resource.json"))
+	})
+	mux.HandleFunc("/api/v1/megames", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		serveJson(w, filepath.Join("testdata", "simplified_games_resource_page_1.json"))
 	})
 	return httptest.NewServer(mux)
+}
+
+func serveJson(w http.ResponseWriter, jsonResponeFilePath string) {
+	response, err := ioutil.ReadFile(jsonResponeFilePath)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
