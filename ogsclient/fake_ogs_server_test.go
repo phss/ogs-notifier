6ddd0c163e2b27f67b6f660a60fa46a9d46882e3
Paulo Schneider <paulo.schneider@gmail.com>
2019-08-10 10:08:23 +0100

    Move test server fixture to file

--- ogsclient/fake_ogs_server_test.go
+++ ogsclient/fake_ogs_server_test.go
@@ -1,8 +1,10 @@
 package ogsclient_test
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,31 +14,13 @@ func fakeOgsServer(t *testing.T) *httptest.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/me", func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "GET", r.Method)
-		response := `{
-			"id": 12345,
-			"username": "someuser",
-			"rating": 948.565,
-			"rating_blitz": 1150,
-			"rating_live": 850.063,
-			"rating_correspondence": 1088.131,
-			"ranking": 18,
-			"ranking_blitz": 20,
-			"ranking_live": 17,
-			"ranking_correspondence": 19,
-			"about": "",
-			"settings": "/api/v1/mesettings",
-			"friends": "/api/v1/mefriends",
-			"games": "/api/v1/megames",
-			"challenges": "/api/v1/mechallenges",
-			"groups": "/api/v1/megroups",
-			"mail": "/api/v1/memail",
-			"tournaments": "/api/v1/metournaments",
-			"vacation": "/api/v1/mevacation",
-			"notifications": "/api/v1/menotifications"
-		}`
+		response, err := ioutil.ReadFile(filepath.Join("testdata", "user_resource.json"))
+		if err != nil {
+			t.Fatal(err)
+		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(response))
+		w.Write(response)
 	})
 	return httptest.NewServer(mux)
 }
