90a3ff0f34a194b2e22c61a7e141e0cca07edea0
Paulo Schneider <paulo.schneider@gmail.com>
2019-08-12 11:19:46 +0100

    Move User to its own file

--- ogsclient/me.go
+++ ogsclient/me.go
@@ -1,28 +1,8 @@
 package ogsclient
 
 import (
-	"fmt"
-
 	"github.com/dghubble/sling"
 )
-
-// User represents an authenticated user resource
-type User struct {
-	ID       int     `json:"id"`
-	Username string  `json:"username"`
-	Rating   float32 `json:"rating"`
-	Ranking  int     `json:"ranking"`
-}
-
-// DisplayRanking converts Ranking into the usual Kyu/Dan ranking.
-func (user User) DisplayRanking() string {
-	switch {
-	case user.Ranking >= 30:
-		return fmt.Sprintf("%d Dan", user.Ranking-29)
-	default:
-		return fmt.Sprintf("%d Kyu", 30-user.Ranking)
-	}
-}
 
 // MeService provides method for accessing resources under /me endpoint
 // of the OGS API
--- ogsclient/me_test.go
+++ ogsclient/me_test.go
@@ -9,36 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMeResource_displayRank(t *testing.T) {
-	tt := []struct {
-		name                   string
-		ranking                int
-		expectedDisplayRanking string
-	}{
-		{name: "total beginner", ranking: 0, expectedDisplayRanking: "30 Kyu"},
-		{name: "ddk", ranking: 18, expectedDisplayRanking: "12 Kyu"},
-		{name: "sdk", ranking: 23, expectedDisplayRanking: "7 Kyu"},
-		{name: "almost dan", ranking: 29, expectedDisplayRanking: "1 Kyu"},
-		{name: "shodan", ranking: 30, expectedDisplayRanking: "1 Dan"},
-		{name: "strong dan", ranking: 36, expectedDisplayRanking: "7 Dan"},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			user := &ogsclient.User{
-				ID:       12345,
-				Username: "someuser",
-				Rating:   123.456,
-				Ranking:  tc.ranking,
-			}
-
-			rank := user.DisplayRanking()
-
-			assert.Equal(t, tc.expectedDisplayRanking, rank)
-		})
-	}
-}
-
 func TestMeService_User(t *testing.T) {
 	server := fakeOgsServer(t)
 	expectedUser := &ogsclient.User{
