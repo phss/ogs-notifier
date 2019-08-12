ecbc732a3408d824bcfca51efe5f62b92ad793df
Paulo Schneider <paulo.schneider@gmail.com>
2019-08-12 11:55:49 +0100

    Render json template for fake OGS responses

--- ogsclient/ogsclient_test.go
+++ ogsclient/ogsclient_test.go
@@ -3,7 +3,6 @@ package ogsclient_test
 import (
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"path/filepath"
@@ -24,7 +23,7 @@ func fakeOgsServer(t *testing.T) *httptest.Server {
 	}
 	mux.HandleFunc("/me", func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "GET", r.Method)
-		serveJson(w, filepath.Join("testdata", "user_resource.json"))
+		serveJsonTemplate(w, filepath.Join("testdata", "user_resource.json"), templateData)
 	})
 	mux.HandleFunc("/me/games", func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "GET", r.Method)
@@ -32,24 +31,17 @@ func fakeOgsServer(t *testing.T) *httptest.Server {
 		if page == "" {
 			page = "1"
 		}
-		filename := filepath.Join("testdata", fmt.Sprintf("simplified_games_resource_page_%s.json", page))
-		template, err := template.ParseFiles(filename)
-		if err != nil {
-			panic(t)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		template.Execute(w, templateData)
+		serveJsonTemplate(w, filepath.Join("testdata", fmt.Sprintf("simplified_games_resource_page_%s.json", page)), templateData)
 	})
 	return server
 }
 
-func serveJson(w http.ResponseWriter, jsonResponeFilePath string) {
-	response, err := ioutil.ReadFile(jsonResponeFilePath)
+func serveJsonTemplate(w http.ResponseWriter, jsonResponseTemplateFilename string, data templateData) {
+	template, err := template.ParseFiles(jsonResponseTemplateFilename)
 	if err != nil {
 		panic(err)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(response)
+	template.Execute(w, data)
 }
