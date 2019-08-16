6e706e92d638127de874f91fc3cf3d0ad2a68f14
Paulo Schneider <paulo.schneider@gmail.com>
2019-08-16 11:20:45 +0100

    Split commands into a separate package

---
+++ cmd/ogscli/commands/root.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ogscli",
+	Short: "ogscli is a command line utility to interact with OGS",
+	Long:  "A command line utility to interact with the Online Go server (https://online-go.com/)",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hi")
+	},
+}
+
+// Execute runs the CLI commands.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- cmd/ogscli/main.go
+++ cmd/ogscli/main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
-)
-
-var rootCmd = &cobra.Command{
-	Use:   "ogscli",
-	Short: "ogscli is a command line utility to interact with OGS",
-	Long:  "A command line utility to interact with the Online Go server (https://online-go.com/)",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hi")
-	},
-}
+import "github.com/phss/ogs-notifier/cmd/ogscli/commands"
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	commands.Execute()
 }
