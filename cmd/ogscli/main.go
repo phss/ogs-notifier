b978dd2c46016d0450c736bcedd4e01c3c2a6d1e
Paulo Schneider <paulo.schneider@gmail.com>
2019-08-16 11:17:07 +0100

    Add root command for ogscli

--- cmd/ogscli/main.go
+++ cmd/ogscli/main.go
@@ -1,51 +1,24 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
-	"text/tabwriter"
 
-	"github.com/phss/ogs-notifier/pkg/oauth"
-	"github.com/phss/ogs-notifier/pkg/ogsclient"
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "ogscli",
+	Short: "ogscli is a command line utility to interact with OGS",
+	Long:  "A command line utility to interact with the Online Go server (https://online-go.com/)",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hi")
+	},
+}
+
 func main() {
-	clientID := flag.String("clientId", "", "")
-	clientSecret := flag.String("clientSecret", "", "")
-	username := flag.String("username", "", "")
-	password := flag.String("password", "", "")
-	flag.Parse()
-
-	oauthConfig := oauth.Config{
-		TokenURL:     "https://online-go.com/oauth2/token/",
-		ClientID:     *clientID,
-		ClientSecret: *clientSecret,
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	oauthClient, err := oauth.PasswordCredentialsClient(oauthConfig, *username, *password)
-	if err != nil {
-		panic(err)
-	}
-
-	ogsClient := ogsclient.NewClient(oauthClient.HTTPClient, "http://online-go.com/api/v1/")
-
-	user, err := ogsClient.Me.User()
-	if err != nil {
-		panic(err)
-	}
-	games, err := ogsClient.Me.Games()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("My ranking is %s\n", user.DisplayRanking())
-	fmt.Println("My current games are:")
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
-	fmt.Fprintln(w, "NAME\tBLACK\tWHITE\tLINK\t")
-	for _, game := range *games {
-		if !game.HasEnded() {
-			fmt.Fprintf(w, "%s\t%s\t%s\thttps://online-go.com/game/%d\n", game.Name, game.Players.Black.Username, game.Players.White.Username, game.ID)
-		}
-	}
-	w.Flush()
 }
